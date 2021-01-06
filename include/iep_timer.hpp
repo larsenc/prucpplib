91157e4ce3a2d376107278251419c8d3c24fe094
larsenc <larsenc@student.chalmers.se>
2021-01-06 21:00:12 +0100

    Implement a simple timer using the IEP peripheral

---
+++ include/iep_timer.hpp
@@ -0,0 +1,44 @@
+#ifndef IEP_TIMER_HPP
+#define IEP_TIMER_HPP
+
+#include "pru_iep.h"
+
+/**
+ * The IEPTimer implements a simple timer using the IEP peripheral.
+ *
+ * The IEPTimer makes sure that CYCLE_LENGTH(5 ns) cycles elapses between consecutive calls to IEPTimer::throttle()
+ *
+ * Limitation: I suspect there will be issues if one tries to use an instance of the IEPTimer class in both PRUs
+ *             since they are acting on the same IEP registers.
+ */
+template<uint32_t CYCLE_LENGTH>
+class IEPTimer
+{
+public:
+    IEPTimer()
+    {
+        resetCounter();
+    }
+
+    void throttle()
+    {
+        // Loop until desired count has passed.
+        while(CT_IEP.TMR_CNT <= CYCLE_LENGTH) {}
+
+        resetCounter();
+    }
+
+private:
+    void resetCounter()
+    {
+        // Set the counter to zero
+        CT_IEP.TMR_CNT = 0x0;
+
+        // Set the global configure to 10001
+        //  - Enable the counter (bit 0)
+        //  - Define the default increment value as 1 (bit 4-7)
+        CT_IEP.TMR_GLB_CFG = 0x11;
+    }
+};
+
+#endif // IEP_TIMER_HPP
